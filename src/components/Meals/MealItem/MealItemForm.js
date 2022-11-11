efc6715aa3b37e1566cd4275f757b7d8771fd992
unknown <fredericreact@gmail.com>
2022-11-12 00:12:51 +0800

    fix form input id

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -3,8 +3,10 @@ import classes from './MealItemForm.module.css'
 
 const MealItemForm = (props) => {
 return <form className={classes.form}>
-    <Input label="Amount" input={{
-        id: 'amount',
+    <Input 
+    label="Amount" 
+    input={{
+        id: 'amount_' + props.id,
         type:'number',
         min:'1',
         max :'5',
